579e42a7ca4a8246d5e1cbe5035decd2af38ec65
jeddaa <fathiato@gmail.com>
2023-03-27 12:24:58 +0100

    fixing linters

--- modules/date.js
+++ modules/date.js
@@ -2,8 +2,13 @@ import { DateTime } from './luxon.js';
 // import { DateTime } from '../node_modules/@types/luxon/index';
 const current = () => {
   // let utcDatetime;
-  let utcDatetime = DateTime.now().toLocaleString({ month: 'long', day: '2-digit', year: 'numeric', hour: '2-digit', minute: '2-digit' });
-  console.log(utcDatetime);
+  const utcDatetime = DateTime.now().toLocaleString({
+    month: 'long',
+    day: '2-digit',
+    year: 'numeric',
+    hour: '2-digit',
+    minute: '2-digit',
+  });
   return utcDatetime;
 };
 export default current;
